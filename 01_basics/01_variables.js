4034ac8e95b9ea104df6976ac387709a48cde51a
Muhammad Ismaeel <103042222+MuhammadIsmaeel2864@users.noreply.github.com>
2024-10-19 07:31:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ismailislamian9@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mardan"
+let accountState;
+//accountId =2  Not Allowed
+
+accountEmail = "khan@gmail.com"
+accountPassword="tiger1234"
+accountCity="Peshawar"
+
+console.log(accountId);
+
+console.table([accountState,accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat code as newer version
+
+// alerr(3+3) // we are node , not browser
+
+// console.log(3+3);
+// console.log("ismaeel");
+
+let name = "ismail";
+let age = 12;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigInt =>
+
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "imail");
+console.log(typeof false);
+console.log(typeof age);
+console.log(typeof null); //type Object
+console.log(typeof undefined); // type Undefined
