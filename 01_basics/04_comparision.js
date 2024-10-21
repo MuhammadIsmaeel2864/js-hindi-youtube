282501b4dd7405e54fba2332a904c4e3c0d40302
Muhammad Ismaeel <103042222+MuhammadIsmaeel2864@users.noreply.github.com>
2024-10-21 05:20:03 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log("2">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+// === is for strict check it also check for its datatypes
+
+console.log("Strict check");
+
+console.log("3" === 4);
+console.log(false === false);
+console.log(0 === false);
+
+console.log(0 == false); // this will give true because it doest a strict check
