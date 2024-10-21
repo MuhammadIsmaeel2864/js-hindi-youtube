2d4f86a578f9fb683b0b74d0eb552b8d962397c4
Muhammad Ismaeel <103042222+MuhammadIsmaeel2864@users.noreply.github.com>
2024-10-21 05:05:42 +0000

    conersion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,24 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueNumber = Number (score);
+// let valueNumber = Number (score);
 
-console.log(typeof(valueNumber));
- console.log(valueNumber);
+// console.log(typeof(valueNumber));
+//  console.log(valueNumber);
  
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log((3+4)*5%3);
+
+// console.log(+"");
+
+ let gameCounter = 100;
+//  gameCounter++;
+++gameCounter;
+ console.log(gameCounter);
+ 
+
